67a63c117984b363763bed097ae22f9bbb35bb80
Mike <mike.johnson@booking.com>
2016-06-03 09:36:49 -0700

    fix backslash bug, improve regex

--- parse.js
+++ parse.js
@@ -7,12 +7,11 @@ module.exports = function(template) {
         "var p=[];with(context){p.push(`" +
        
         template
-            .replace(/\\'/g, "\\\\'")
+            .replace(/\\/g, "\\\\")
             .replace(/`/g, "\\`")
             .replace(/<::(?!\s*}.*?::>)(?!.*{\s*::>)(.*?)::>/g, "`);try{p.push($1)}catch(e){}p.push(`")
-            .replace(/<::\s*(.*?)\s*::>/g, "`);$1\np.push(`")
-            .replace(/<:(?!\s*}.*?:>)(?!.*{\s*:>)(.*?):>/g, "`);try{p.push(manila.e($1))}catch(e){}p.push(`")
-            .replace(/<:\s*(.*?)\s*:>/g, "`);$1\np.push(`")
+            .replace(/<::?(?!\s*}.*?::?>)(?!.*{\s*::?>)(.*?)::?>/g, "`);try{p.push(manila.e($1))}catch(e){}p.push(`")
+            .replace(/<::?(.*?)::?>/g, "`);$1\np.push(`")
 
       + "`);}return p.join('');");
 };
